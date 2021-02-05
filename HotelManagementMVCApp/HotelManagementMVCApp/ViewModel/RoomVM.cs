4f76f56926602af1c1e23f90370d24659f6a90d7
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-06 01:12:33 +0300

    "Update RoomVM.cs and Room/Index.cshtml"

--- HotelManagementMVCApp/HotelManagementMVCApp/ViewModel/RoomVM.cs
+++ HotelManagementMVCApp/HotelManagementMVCApp/ViewModel/RoomVM.cs
@@ -12,22 +12,33 @@ namespace HotelManagementMVCApp.ViewModel
         public int roomId { get; set; }
 
         [StringLength(10)]
+        [Display(Name = "Room Number*")]
+        [Required]
         public string roomNumber { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Room Image*")]
+        [Required]
         public string roomImage { get; set; }
 
+        [Display(Name = "Room Price*")]
+        [Required]
         public int? roomPrice { get; set; }
 
+        [Display(Name = "Booking Status")]
         public int? bookingStatusId { get; set; }
 
+        [Display(Name = "Room Type")]
         public int? roomTypeId { get; set; }
 
+        [Display(Name = "Room Capacity")]
         public int? roomCapacity { get; set; }
 
         [StringLength(50)]
+        [Display(Name = "Room Description")]        
         public string roomDescription { get; set; }
 
+        [Display(Name = "Is Active")]       
         public bool? isActive { get; set; }
 
         public HttpPostedFileBase Image { get; set; }

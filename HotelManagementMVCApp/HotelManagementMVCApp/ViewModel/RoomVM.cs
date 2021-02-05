a3f50cdfedc74d13939537353a9ef602eece2380
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-06 00:01:16 +0300

    "Added ViewModel/RoomVM.cs"

---
+++ HotelManagementMVCApp/HotelManagementMVCApp/ViewModel/RoomVM.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HotelManagementMVCApp.ViewModel
+{
+    public class RoomVM
+    {
+        public int roomId { get; set; }
+
+        [StringLength(10)]
+        public string roomNumber { get; set; }
+
+        [StringLength(50)]
+        public string roomImage { get; set; }
+
+        public int? roomPrice { get; set; }
+
+        public int? bookingStatusId { get; set; }
+
+        public int? roomTypeId { get; set; }
+
+        public int? roomCapacity { get; set; }
+
+        [StringLength(50)]
+        public string roomDescription { get; set; }
+
+        public bool? isActive { get; set; }
+    }
+}

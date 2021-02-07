7e188b763960677094d5b461a6310b9129b98737
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-07 22:42:10 +0300

    "Added CustomerController.cs, CustomerVM.cs, CustomerDetailsVM.cs, and Customer/Index.cshtml"

---
+++ HotelManagementMVCApp/HotelManagementMVCApp/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using HotelManagementMVCApp.Models;
+using HotelManagementMVCApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelManagementMVCApp.Controllers
+{
+    public class CustomerController : Controller
+    {
+        private Db db;
+        public CustomerController()
+        {
+            db = new Db();
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+
+        // GET: Customer
+        public ActionResult Index()
+        {
+            CustomerVM customerVM = new CustomerVM();
+
+            customerVM.genderType = (from obj in db.Gender
+                                          select new SelectListItem()
+                                          {
+                                              Text = obj.genderTypeName,
+                                              Value = obj.genderId.ToString()
+                                          }).ToList();            
+            return View(customerVM);
+        }
+    }
+}
---
+++ HotelManagementMVCApp/HotelManagementMVCApp/ViewModel/CustomerVM.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelManagementMVCApp.ViewModel
+{
+    public class CustomerVM
+    {
+        public int customerId { get; set; }
+
+        [StringLength(10)]
+        [Display(Name = "First Name*")]
+        [Required(ErrorMessage = "First Name is required.")]
+        public string firstName { get; set; }
+
+        [Display(Name = "Last Name*")]
+        [Required(ErrorMessage = "Last Name is required.")]        
+        public string lastName { get; set; }
+
+        [Display(Name = "Phone Number*")]
+        [Required(ErrorMessage = "Phone Number is required.")]
+        public string phoneNumber { get; set; }
+
+        [Display(Name = "Customer Address*")]
+        [Required(ErrorMessage = "Customer Address is required.")]
+        public string address { get; set; }
+
+        [Display(Name = "Gender*")]
+        [Required(ErrorMessage = "Gender is required.")]
+        public int? genderId { get; set; }
+
+        public List<SelectListItem> genderType { get; set; }
+    }
+}

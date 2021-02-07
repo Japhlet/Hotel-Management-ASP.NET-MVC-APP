8e1bd2537127d8ec744cff11763f14c224899911
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-08 00:40:31 +0300

    "Implemented functionality to ADD, EDIT and DELETE Gender Type"

---
+++ HotelManagementMVCApp/HotelManagementMVCApp/Controllers/GenderController.cs
@@ -0,0 +1,94 @@
+﻿using HotelManagementMVCApp.Models;
+using HotelManagementMVCApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelManagementMVCApp.Controllers
+{
+    public class GenderController : Controller
+    {
+        private Db db;
+        public GenderController()
+        {
+            db = new Db();
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+
+        // GET: Gender
+        public ActionResult Index()
+        {
+            GenderVM genderVM = new GenderVM();
+
+            return View(genderVM);
+        }
+
+        [HttpPost]
+        public ActionResult Index(GenderVM genderVM)
+        {
+            string message = String.Empty;
+
+            if (genderVM.genderId == 0)
+            {
+                Gender gender = new Gender()
+                {
+                    genderTypeName = genderVM.genderTypeName,                    
+                    isDeleted = false
+                };
+
+                db.Gender.Add(gender);
+                message = "Gender Type saved successfully.";
+            }
+            else
+            {
+                Gender genderToEdit = db.Gender.Single(m => m.genderId == genderVM.genderId);
+
+                genderToEdit.genderTypeName = genderVM.genderTypeName;                
+                genderToEdit.isDeleted = false;
+
+                message = "Gender Type updated successfully.";
+            }
+
+            db.SaveChanges();
+
+            return Json(new { message = message, success = true }, JsonRequestBehavior.AllowGet);
+        }
+
+        public PartialViewResult GetAllGenderTypes()
+        {
+            IEnumerable<GenderDetailsVM> ListOfGenderDetailsVM =
+                (from objGender in db.Gender                 
+                 where objGender.isDeleted == false
+                 select new GenderDetailsVM()
+                 {                     
+                     genderType = objGender.genderTypeName,
+                     genderId = objGender.genderId
+                 }).ToList();
+            return PartialView("_GenderDetailsPartial", ListOfGenderDetailsVM);
+        }
+
+        [HttpGet]
+        public JsonResult EditGenderDetails(int genderId)
+        {
+            var result = db.Gender.Single(m => m.genderId == genderId);
+
+            return Json(result, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpGet]
+        public JsonResult DeleteGenderDetails(int genderId)
+        {
+            string msg = String.Empty;
+
+            Gender gender = db.Gender.Single(m => m.genderId == genderId);
+            gender.isDeleted = true;
+
+            db.SaveChanges();
+            msg = "Record successfully deleted.";
+
+            return Json(new { message = msg, success = true }, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ HotelManagementMVCApp/HotelManagementMVCApp/ViewModel/GenderVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace HotelManagementMVCApp.ViewModel
+{
+    public class GenderVM
+    {
+        public int genderId { get; set; }
+
+        [StringLength(10)]
+        [Display(Name = "Gender:*")]
+        [Required(ErrorMessage = "Gender is required.")]
+        public string genderTypeName { get; set; }
+    }
+}

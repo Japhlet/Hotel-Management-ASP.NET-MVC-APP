1fa8b4bfa3037300fe7a0339195f47762a07a459
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-07 22:09:04 +0300

    "Added form input validation and validation error messages"

--- HotelManagementMVCApp/HotelManagementMVCApp/ViewModel/RoomVM.cs
+++ HotelManagementMVCApp/HotelManagementMVCApp/ViewModel/RoomVM.cs
@@ -13,29 +13,34 @@ namespace HotelManagementMVCApp.ViewModel
 
         [StringLength(10)]
         [Display(Name = "Room Number*")]
-        [Required]
+        [Required(ErrorMessage ="Room number is required.")]
         public string roomNumber { get; set; }
 
         [StringLength(50)]
-        [Display(Name = "Room Image*")]
-        [Required]
+        [Display(Name = "Room Image")]        
         public string roomImage { get; set; }
 
         [Display(Name = "Room Price*")]
-        [Required]
+        [Required(ErrorMessage = "Room price is required.")]
+        [Range(500, 10000, ErrorMessage = "Room price should be equal or greater than {1}")]
         public int? roomPrice { get; set; }
 
-        [Display(Name = "Booking Status")]
+        [Display(Name = "Booking Status*")]
+        [Required(ErrorMessage = "Booking status is required.")]
         public int? bookingStatusId { get; set; }
 
-        [Display(Name = "Room Type")]
+        [Display(Name = "Room Type*")]
+        [Required(ErrorMessage = "Room type is required.")]
         public int? roomTypeId { get; set; }
 
-        [Display(Name = "Room Capacity")]
+        [Display(Name = "Room Capacity*")]
+        [Required(ErrorMessage = "Room capacity is required.")]
+        [Range(1,8, ErrorMessage ="Room capacity should be equal or greater than {1}")]
         public int? roomCapacity { get; set; }
 
         [StringLength(50)]
-        [Display(Name = "Room Description")]        
+        [Display(Name = "Room Description*")]
+        [Required(ErrorMessage = "Room description is required.")]
         public string roomDescription { get; set; }
 
         [Display(Name = "Is Active")]       

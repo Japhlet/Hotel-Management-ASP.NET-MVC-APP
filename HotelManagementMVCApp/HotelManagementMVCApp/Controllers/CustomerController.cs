00c187424311f298a827ab8df1feefa1e0515590
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-07 23:13:48 +0300

    "Implemented add customer functionality"

--- HotelManagementMVCApp/HotelManagementMVCApp/Controllers/CustomerController.cs
+++ HotelManagementMVCApp/HotelManagementMVCApp/Controllers/CustomerController.cs
@@ -30,5 +30,42 @@ namespace HotelManagementMVCApp.Controllers
                                           }).ToList();            
             return View(customerVM);
         }
+
+        [HttpPost]
+        public ActionResult Index(CustomerVM customerVM)
+        {
+            string message = String.Empty;
+
+            if (customerVM.customerId == 0)
+            {
+                Customer customer = new Customer()
+                {
+                    firstName = customerVM.firstName,
+                    lastName = customerVM.lastName,
+                    phoneNumber = customerVM.phoneNumber,
+                    address = customerVM.address,
+                    genderId = customerVM.genderId                    
+                };
+
+                db.Customer.Add(customer);
+                message = "Customer saved successfully.";
+            }
+            else
+            {
+                Customer customerToEdit = db.Customer.Single(m => m.customerId == customerVM.customerId);
+
+                customerToEdit.firstName = customerVM.firstName;
+                customerToEdit.lastName = customerVM.lastName;
+                customerToEdit.phoneNumber = customerVM.phoneNumber;
+                customerToEdit.address = customerVM.address;
+                customerToEdit.genderId = customerVM.genderId;
+
+                message = "Customer updated successfully.";
+            }
+
+            db.SaveChanges();
+
+            return Json(new { message = message, success = true }, JsonRequestBehavior.AllowGet);
+        }
     }
 }

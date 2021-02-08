e66e15d53f4c2ed7c8b261d15ebac208172d8a73
japhlet <etyangemodojaphlet@yahoo.com>
2021-02-08 16:00:03 +0300

    "Added Book Room Button"

---
+++ HotelManagementMVCApp/HotelManagementMVCApp/Controllers/RoomBookingController.cs
@@ -0,0 +1,43 @@
+﻿using HotelManagementMVCApp.Models;
+using HotelManagementMVCApp.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelManagementMVCApp.Controllers
+{
+    public class RoomBookingController : Controller
+    {
+        private Db db;
+        public RoomBookingController()
+        {
+            db = new Db();
+            db.Configuration.ProxyCreationEnabled = false;
+        }
+
+        // GET: RoomBooking
+        public ActionResult Index()
+        {
+            RoomBookingVM roomBookingVM = new RoomBookingVM();
+
+            roomBookingVM.ListOfCustomers = (from objCustomer in db.Customer
+                                             where objCustomer.isDeleted == false
+                                             select new SelectListItem()
+                                          {
+                                              Text = objCustomer.firstName +" "+ objCustomer.lastName,
+                                              Value = objCustomer.customerId.ToString()
+                                          }).ToList();
+
+            roomBookingVM.ListOfAssignedRooms = (from objRoom in db.Room
+                                                 where objRoom.bookingStatusId == 1
+                                     select new SelectListItem()
+                                     {
+                                         Text = objRoom.roomNumber,
+                                         Value = objRoom.roomId.ToString()
+                                     }).ToList();
+            return View(roomBookingVM);
+        }
+    }
+}
---
+++ HotelManagementMVCApp/HotelManagementMVCApp/ViewModel/RoomBookingVM.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace HotelManagementMVCApp.ViewModel
+{
+    public class RoomBookingVM
+    {
+        public int bookingId { get; set; }
+
+        [Display(Name = "Customer*")]
+        [Required(ErrorMessage = "Customer is required.")]
+        public int? customerId { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        [Display(Name = "Book From*")]
+        [Required(ErrorMessage = "Booking start date is required.")]        
+        public DateTime? bookingFrom { get; set; }
+
+        [Column(TypeName = "datetime2")]
+        [Display(Name = "Book To*")]
+        [Required(ErrorMessage = "Booking end date is required.")]
+        public DateTime? bookingTo { get; set; }
+
+        [Display(Name = "Assign Room*")]
+        [Required(ErrorMessage = "Assign Room is required.")]
+        public int? assignRoomId { get; set; }
+
+        [Display(Name = "Number of Occupants*")]
+        [Required(ErrorMessage = "Number of occupants is required.")]
+        public int? numberOfOccupants { get; set; }
+
+        [Display(Name = "Total Amount*")]        
+        public int? totalAmount { get; set; }
+
+        public bool? isDeleted { get; set; }
+
+        public List<SelectListItem> ListOfCustomers { get; set; }
+        public List<SelectListItem> ListOfAssignedRooms { get; set; }
+    }
+}
